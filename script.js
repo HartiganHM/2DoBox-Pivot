664f7cc40428de456f8fb79757205c0c1a061b47
katiescruggs <katie.e.scruggs@gmail.com>
2017-09-13 15:59:09 -0600

    one event listener for all importance filtering

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
-//****Event Listeners****
- $(document).ready(function(){
-  $('.show-completed-button').hide();
-  getFromStorage();
-  enableControlButtons();
- });
+$(document).ready(function(){
+ $('.show-completed-button').hide();
+ getFromStorage();
+ enableControlButtons();
+});
+
 $(document).on('blur', '.card-title', editCardTitle);
 $(document).on('blur', '.card-body', editCardBody);
 $('.clear-all-button').on('click', clearAllCards);
 $('.save-button').on('click', createCard);
 $('.show-more').on('click', showMoreCards);
-$('.critical-btn').on('click', filterCritical);
-$('.high-btn').on('click', filterHigh);
-$('.normal-btn').on('click', filterNormal);
-$('.low-btn').on('click', filterLow);
-$('.none-btn').on('click', filterNone);
+
+$('.importance').on('click', filterImportance);
+
+// $('.critical-btn').on('click', filterCritical);
+// $('.high-btn').on('click', filterHigh);
+// $('.normal-btn').on('click', filterNormal);
+// $('.low-btn').on('click', filterLow);
+// $('.none-btn').on('click', filterNone);
+
 $('.user-title, .user-body').on('keyup', characterCounter);
 $('.user-title, .user-body').on('keyup', enableSaveButton);
 $('.search').on('keyup', searchCards);
@@ -22,7 +26,21 @@ $('main').on('click', '.up-vote, .down-vote', qualityHandler);
 $('main').on('click', '.completed', completedCard);
 $('.show-completed-button').on('click', showCompletedCards);
 
-//****Card Object****
+function getFromStorage() {
+ keys = Object.keys(localStorage);
+ for (var i = 0; i < keys.length; i++) {
+   cardTemplate(JSON.parse(localStorage.getItem(keys[i])));
+ } 
+ var domCards = $('.card:visible')
+ if(domCards.length > 10) {
+   hideOldCards();
+ }
+}
+
+function saveToStorage(card) {
+  localStorage.setItem(card.id, JSON.stringify(card));
+}
+
 function Card(object) {
   this.title = object.title;
   this.body = object.body;
@@ -31,81 +49,20 @@ function Card(object) {
   this.completed = false;
 }
 
-function tweakQuality(direction, currentQuality) {
-  var qualityArray = ['none', 'low', 'normal', 'high', 'critical'];
-  var index = qualityArray.indexOf(currentQuality);
-  direction === true? index++ : index--;
-
-  if(index > 4) { index = 4; }
-  if(index < 0) { index = 0; }
-
-  return qualityArray[index];
+function createCard(event) {
+  event.preventDefault();
+  var title = $('.user-title').val();
+  var body = $('.user-body').val();
+  var theCard = new Card({title, body});
+  saveToStorage(theCard);
+  displayCard(theCard);
 }
 
-function qualityHandler(event) {
-  var $cardQuality = $(this).siblings('.level');
-  var newQuality = tweakQuality($(this).hasClass('up-vote'), $cardQuality.text());
-  var storedCard = eventGetCard(event);
-  $cardQuality.text(newQuality);
-  storedCard.quality = newQuality || 'normal';
-  saveToStorage(storedCard);
-}
-
-function deleteCard(event) {
-  var articleElement = $(event.target).closest('article');
-  var id = articleElement.prop('id');
-  articleElement.remove();
-  localStorage.removeItem(id);
-  enableControlButtons();
-}
-
-function eventGetCard(event) {
-  var articleElement = $(event.target).closest('article')
-  var id = articleElement.prop('id');
-  return JSON.parse(localStorage.getItem(id));
-}
-
- function getFromStorage() {
-  keys = Object.keys(localStorage);
-  for (var i = 0; i < keys.length; i++) {
-    cardTemplate(JSON.parse(localStorage.getItem(keys[i])));
-  } 
-  var domCards = $('.card:visible')
-  if(domCards.length > 10) {
-    hideOldCards();
-  }
-}
-
-function showCompletedCards() {
-  keys = Object.keys(localStorage);
-  for (var i = 0; i < keys.length; i++) {
-    var parsedCard = JSON.parse(localStorage.getItem(keys[i]));
-    if (parsedCard.completed === true) {
-    prependCompleted(parsedCard);
-    }
-  }
+function displayCard(card) {
+  $('main').prepend(cardTemplate(card));
+  resetInputs();
   hideOldCards();
-  $('.show-completed-button').hide(); 
-}
-
-function saveToStorage(card) {
-  localStorage.setItem(card.id, JSON.stringify(card));
-}
-
-//****Functions****
-function prependCompleted(card) {
-  $('main').prepend(
-        `
-          <article class="card" id=${card.id}>
-            <h2 contenteditable=true class="card-title">${card.title}</h2>
-            <button class="delete"></button>
-            <p contenteditable=true class="card-body">${card.body}</p>
-            <button class="up-vote"></button>
-            <button class="down-vote"></button>
-            <p class="quality">quality: </p><p class="level">${card.quality}</p><p class="completed">Completed</p>
-          </article>
-        `
-      )
+  enableControlButtons();
 }
 
 function cardTemplate(card) {
@@ -128,6 +85,65 @@ function cardTemplate(card) {
   }
 }
 
+function prependCompleted(card) {
+  $('main').prepend(
+        `
+          <article class="card" id=${card.id}>
+            <h2 contenteditable=true class="card-title">${card.title}</h2>
+            <button class="delete"></button>
+            <p contenteditable=true class="card-body">${card.body}</p>
+            <button class="up-vote"></button>
+            <button class="down-vote"></button>
+            <p class="quality">quality: </p><p class="level">${card.quality}</p><p class="completed">Completed</p>
+          </article>
+        `
+      )
+}
+
+function eventGetCard(event) {
+  var articleElement = $(event.target).closest('article')
+  var id = articleElement.prop('id');
+  return JSON.parse(localStorage.getItem(id));
+}
+
+function qualityHandler(event) {
+  var $cardQuality = $(this).siblings('.level');
+  var newQuality = tweakQuality($(this).hasClass('up-vote'), $cardQuality.text());
+  var storedCard = eventGetCard(event);
+  $cardQuality.text(newQuality);
+  storedCard.quality = newQuality || 'normal';
+  saveToStorage(storedCard);
+}
+
+function tweakQuality(direction, currentQuality) {
+  var qualityArray = ['none', 'low', 'normal', 'high', 'critical'];
+  var index = qualityArray.indexOf(currentQuality);
+  direction === true? index++ : index--;
+  if(index > 4) { index = 4; }
+  if(index < 0) { index = 0; }
+  return qualityArray[index];
+}
+
+function deleteCard(event) {
+  var articleElement = $(event.target).closest('article');
+  var id = articleElement.prop('id');
+  articleElement.remove();
+  localStorage.removeItem(id);
+  enableControlButtons();
+}
+
+function showCompletedCards() {
+  keys = Object.keys(localStorage);
+  for (var i = 0; i < keys.length; i++) {
+    var parsedCard = JSON.parse(localStorage.getItem(keys[i]));
+    if (parsedCard.completed === true) {
+    prependCompleted(parsedCard);
+    }
+  }
+  hideOldCards();
+  $('.show-completed-button').hide(); 
+}
+
 function clearAllCards(event) {
   event.preventDefault();
   $('article').remove();
@@ -142,22 +158,6 @@ function completedCard(event) {
   storedCard.completed = true;
   $(this).parent().addClass('completed-card');
   saveToStorage(storedCard);
-}
-
-function createCard(event) {
-  event.preventDefault();
-  var title = $('.user-title').val();
-  var body = $('.user-body').val();
-  var theCard = new Card({title, body});
-  saveToStorage(theCard);
-  displayCard(theCard);
-}
-
-function displayCard(card) {
-  $('main').prepend(cardTemplate(card));
-  resetInputs();
-  hideOldCards();
-  enableControlButtons();
 }
 
 function editCardBody(event){
@@ -226,7 +226,8 @@ function searchCards() {
 function searchFilter() {
   var results = [];
   $('.card').each(function(index, card){
-    if ($(this).children('.card-title').text().toLowerCase().includes($('.search').val().toLowerCase()) || $(this).children('.card-body').text().toLowerCase().includes($('.search').val().toLowerCase())) {
+    if ($(this).children('.card-title').text().toLowerCase().includes($('.search').val().toLowerCase()) 
+        || $(this).children('.card-body').text().toLowerCase().includes($('.search').val().toLowerCase())) {
       results.push(card);
     }
   });
@@ -234,7 +235,6 @@ function searchFilter() {
   return results;
 }
 
-//****Show More Cards****
 function displayFilter(results) {
   $('.card').hide();
   $(results).show();
@@ -257,8 +257,8 @@ function showMoreCards() {
   hideShowMore();
 }
 
-//****Filter Importance****
-function filterImportance(importance) {
+function filterImportance() {
+  var importance = $(this).text();
   var results = [];
     $('.card').each(function(index, card){
       if ($(this).children('.level').text().includes(importance)) {
@@ -268,44 +268,9 @@ function filterImportance(importance) {
   displayFilter(results);
 }
 
-function filterCritical() {
-  filterImportance('critical');
-}
-
-function filterHigh() {
-  filterImportance('high');
-}
-
-function filterNormal() {
-  filterImportance('normal');
-}
-
-function filterLow() {
-  filterImportance('low');
-}
-
-function filterNone() {
-  filterImportance('none');
-}
-
-//****Character Counter****
 function characterCounter() {
   $('.title-chars').text(($('.user-title').val().length));
   $('.body-chars').text(($('.user-body').val().length));  
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
